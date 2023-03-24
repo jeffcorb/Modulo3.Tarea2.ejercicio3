fac8e23ba37aa74373026d949412cabe5f82a197
jeffcorb <jefferson.corbs@gmail.com>
2023-03-23 19:53:26 -0500

    Agregando Clases

---
+++ Simple.java
@@ -0,0 +1,8 @@
+public class Simple {
+    public static void main(String[] args) {
+        var num1= 2;
+        var num2= 3;
+        var resultado = num1*num2;
+        System.out.println("La multiplicacion es: " + resultado);
+    }
+}
